63374d8d1b80b39048bf8ba746c87b76ca341667
mark-scherer <mark.s.scherer@vanderbilt.edu>
2018-06-18 17:34:12 -0500

    added ITv2

---
+++ ITv2/DiagnosticsDriver.cs
@@ -0,0 +1,97 @@
+﻿// Visualizes process diagnostics
+// Mark Scherer, June 2018
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(VertexDriver))]
+public class DiagnosticsDriver : MonoBehaviour
+{
+    // Inspector variables
+    public Material TargetMaterial;
+    public float TargetDistance = 1f;
+    public float TargetSize = 0.01f;
+    public Vector3 DiagnosticsPosition = new Vector3(0, -0.5f, 1);
+    public GameObject DiagnosticsText;
+    public GameObject DiagnosticsBackground;
+
+    // other variables
+    private VertexDriver VD;
+    private Vector3 TargetMin, TargetMax;
+    private float TargetI, TargetJ;
+    private GameObject TargetContainer, Diagnostics;
+    private Vector3 TargetBotLeft, TargetBotRight, TargetTopLeft, TargetTopRight;
+
+	
+    // Use this for initialization
+	void Start () {
+        VD = GetComponent<VertexDriver>();
+
+        // create target
+        TargetI = (float)(TargetDistance * Math.Tan(DegToRad(VD.TargetFOV.x / 2.0)));
+        TargetJ = (float)(TargetDistance * Math.Tan(DegToRad(VD.TargetFOV.y / 2.0)));
+        TargetBotLeft = new Vector3(-TargetI, -TargetJ, TargetDistance);
+        TargetBotRight = new Vector3(TargetI, -TargetJ, TargetDistance);
+        TargetTopLeft = new Vector3(-TargetI, TargetJ, TargetDistance);
+        TargetTopRight = new Vector3(TargetI, TargetJ, TargetDistance);
+        TargetContainer = new GameObject();
+        TargetContainer.name = "TargetContainer";
+        VertexDriver.DrawLine(TargetBotLeft, TargetBotRight, TargetContainer, TargetSize, TargetMaterial, "bottom");
+        VertexDriver.DrawLine(TargetBotRight, TargetTopRight, TargetContainer, TargetSize, TargetMaterial, "right");
+        VertexDriver.DrawLine(TargetTopRight, TargetTopLeft, TargetContainer, TargetSize, TargetMaterial, "top");
+        VertexDriver.DrawLine(TargetTopLeft, TargetBotLeft, TargetContainer, TargetSize, TargetMaterial, "left");
+
+        // create diagnostics panel
+        Diagnostics = new GameObject();
+        Diagnostics.transform.position = DiagnosticsPosition;
+        DiagnosticsText.transform.parent = Diagnostics.transform;
+        DiagnosticsText.transform.position = Diagnostics.transform.TransformPoint(DiagnosticsText.transform.position);
+        DiagnosticsBackground.transform.parent = Diagnostics.transform;
+        DiagnosticsBackground.transform.position = Diagnostics.transform.
+            TransformPoint(DiagnosticsBackground.transform.position);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // update target position
+        TargetContainer.transform.position = Camera.main.transform.position;
+        TargetContainer.transform.eulerAngles = Camera.main.transform.eulerAngles;
+        // bottom
+        LineRenderer bottom = TargetContainer.transform.Find("bottom").GetComponent<LineRenderer>();
+        Vector3[] bottomPos = { TargetContainer.transform.TransformPoint(TargetBotLeft),
+            TargetContainer.transform.TransformPoint(TargetBotRight) };
+        bottom.SetPositions(bottomPos);
+        // right
+        LineRenderer right = TargetContainer.transform.Find("right").GetComponent<LineRenderer>();
+        Vector3[] rightPos = { TargetContainer.transform.TransformPoint(TargetBotRight),
+            TargetContainer.transform.TransformPoint(TargetTopRight) };
+        right.SetPositions(rightPos);
+        // top
+        LineRenderer top = TargetContainer.transform.Find("top").GetComponent<LineRenderer>();
+        Vector3[] topPos = { TargetContainer.transform.TransformPoint(TargetTopRight),
+            TargetContainer.transform.TransformPoint(TargetTopLeft) };
+        top.SetPositions(topPos);
+        // left
+        LineRenderer left = TargetContainer.transform.Find("left").GetComponent<LineRenderer>();
+        Vector3[] leftPos = { TargetContainer.transform.TransformPoint(TargetTopLeft),
+            TargetContainer.transform.TransformPoint(TargetBotLeft) };
+        left.SetPositions(leftPos);
+
+        // control diagnostics text
+        String DMessage = String.Format("In View: {0} \t\t Out of View: {1}",
+            VD.Inter.InViewCount, VD.Inter.OutViewCount);
+        DiagnosticsText.GetComponent<TextMesh>().text = DMessage;
+    }
+
+    private static double RadToDeg(double rad)
+    {
+        return rad * (180.0 / Math.PI);
+    }
+
+    private static double DegToRad(double deg)
+    {
+        return deg * (Math.PI / 180.0);
+    }
+}
---
+++ ITv2/Intersector.cs
@@ -0,0 +1,311 @@
+﻿/// Intersector
+/// Class for calculating resultant PointValues when 2D Raster is project along a Frustum at a Mesh.
+/// Content:
+/// PointValue (struct)
+/// ViewVector (struct)
+/// Frustum (struct)
+/// Intersector (class)
+
+/// NOTE: Tried to make Intersector:Intersection method generic so it was ambivalent to type of img. 
+/// This caused strange Unity build errors when accessing and mutating values within cells of ocGrid.
+/// Currently set to byte, if img data type changes will have to manually change code.
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Generic spatially-aware values.
+/// </summary>
+public struct PointValue<T>
+{
+    public Vector3 Point { get; private set; }
+    public T Value { get; private set; }
+
+    public PointValue(Vector3 myPoint, T myValue)
+    {
+        Point = myPoint;
+        Value = myValue;
+    }
+
+    /// <summary>
+    /// Same as constructor, except no new memory allocation.
+    /// </summary>
+    public void Update(Vector3 newPoint, T newValue)
+    {
+        Point = newPoint;
+        Value = newValue;
+    }
+}
+
+/// <summary>
+/// Vector for relative angle of (x,y,z) vector relative to 2D plane. Vector must originate at origin.
+/// For 2D plane = XY plane, positive Z is forwards, 2D plane coordinates i,j:
+/// Theta (i): angle of vector with YZ plane
+/// Phi (j): angle of vector with XZ plane
+/// </summary>
+public struct ViewVector
+{
+    public double Theta { get; private set; }
+    public double Phi { get; private set; }
+
+    public ViewVector(double myTheta, double myPhi)
+    {
+        Theta = myTheta;
+        Phi = myPhi;
+    }
+
+    /// <summary>
+    /// Creates ViewVector from spatialVector.
+    /// Note: spatialVector MUST originate from origin.
+    /// </summary>
+    public ViewVector(Vector3 spatialVector)
+    {
+        Theta = Intersector.RadToDeg(Intersector.AdjAtanTheta(spatialVector.x, spatialVector.z));
+        Phi = Intersector.RadToDeg(Intersector.AdjAtanPhi(spatialVector));
+    }
+}
+
+/// <summary>
+/// Spatially-aware view field of a sensor.
+/// FOV defines full angle of frustum field of view, centered at transform. 
+/// </summary>
+public struct Frustum
+{
+    public Transform Transform;
+    public ViewVector FOV { get; private set; }
+
+    public Frustum(Transform myTransform, ViewVector myFOV)
+    {
+        Transform = myTransform;
+        FOV = myFOV;
+    }
+}
+
+/// <summary>
+/// Cell of Occlusion grid using closest-in-grid occlusion approximation strategy.
+/// </summary>
+public struct OcclusionCell<T>
+{
+    public PointValue<T> pv;
+    public float distance;
+    public bool nullCell;
+}
+
+
+/// <summary>
+/// Calculates resultant PointValues when 2D Raster is projected along a Frustum at a Mesh.
+/// Mesh represented by list of vertices.
+/// NOTE: does not currently account for occlusion.
+/// </summary>
+public class Intersector : HoloToolkit.Unity.Singleton<Intersector>
+{
+    /// <summary>
+    /// Metadata: how many vertices were in passed Frustum for last call to Intersection().
+    /// </summary>
+    public int VerticesInView { get; private set; }
+
+    /// <summary>
+    /// Metadata: how many vertices were in passed Frustum and non-occluded for last call to Intersection().
+    /// </summary>
+    public int NonOccludedVertices { get; private set; }
+
+    public int InViewCount { get; private set; }
+    public int OutViewCount { get; private set; }
+
+    /// pre-declarations of variables in Intersection() for memory efficiency
+    private int ImgPCi, ImgPCj, i, j, iOc, jOc, iImg, jImg;
+    private List<PointValue<byte>> Result = new List<PointValue<byte>>();
+    private Vector3 Pworld, Plocal;
+    private ViewVector Vlocal;
+
+    /* OLD INTERSECTION VERSION
+    /// <summary>
+    /// Calculates resultant PointValues when 2D Raster (img) is projected along Frustum at Mesh (vertices).
+    /// Mesh represented by list of vertices.
+    /// NOTE 1: does not currently account for occlusion.
+    /// NOTE 2: projection's FOV should be full FOV angles, not half-angles.
+    /// NOTE 3: Attempted to make generic. See note at begining of file.
+    /// </summary>
+    public List<PointValue<byte>> Intersection(Frustum projection, byte[,] img, List<Vector3> vertices)
+    {
+        /// setup
+        ImgPCi = img.GetLength(0);
+        ImgPCj = img.GetLength(1);
+        VerticesInView = 0;
+
+        /// update occlusion grid
+        /// Cannot predeclare OcPixels because cannot add Dictionary components in declaration.
+        Dictionary<string, int> OcPixels = RequiredGrid(projection.FOV);
+        // Predeclaration of ocGrid has no benefit due to array type an dynamic size.
+        OcclusionCell<byte>[,] ocGrid = new OcclusionCell<byte>[OcPixels["i"], OcPixels["j"]];
+        for (i = 0; i < OcPixels["i"]; i++)
+        {
+            for (j = 0; j < OcPixels["j"]; j++)
+            {
+                ocGrid[i, j].nullCell = true;
+            }
+        }
+
+        /// try each vertex
+        for (i = 0; i < vertices.Count; i++)
+        {
+            /// calculate position vector (world space)
+            Vector(projection.Transform.position, vertices[i], ref Pworld);
+
+            /// convert position vector (world space) to position vector (Frustum space)
+            Plocal = projection.Transform.InverseTransformVector(Pworld);
+
+            /// convert position vector (Frustum space) to view vector (Frustum space)
+            Vlocal.Update(Plocal);
+
+            /// check if view vector is within Frustum FOV
+            if (Math.Abs(Vlocal.Theta) < projection.FOV.Theta / 2.0 &&
+                Math.Abs(Vlocal.Phi) < projection.FOV.Phi / 2.0)
+            {
+                VerticesInView++;
+
+                /// map view vector to occlusion grid
+                iOc = (int)(OcPixels["i"] / 2 + OcPixels["i"] * (Vlocal.Theta / projection.FOV.Theta));
+                jOc = (int)(OcPixels["j"] / 2 + OcPixels["j"] * (Vlocal.Phi / projection.FOV.Phi));
+
+                /// add to occlusion grid as new PointValue if not occluded
+
+                if (ocGrid[iOc, jOc].nullCell || Pworld.magnitude < ocGrid[iOc, jOc].distance)
+                {
+                    /// map view vector to img pixel grid
+                    iImg = (int)(ImgPCi / 2 + ImgPCi * (Vlocal.Theta / projection.FOV.Theta));
+                    jImg = (int)(ImgPCj / 2 + ImgPCj * (Vlocal.Phi / projection.FOV.Phi));
+
+                    /// update occlusion grid
+                    ocGrid[iOc, jOc].pv.Update(vertices[i], img[iImg, jImg]);
+                    ocGrid[iOc, jOc].distance = Pworld.magnitude;
+                    ocGrid[iOc, jOc].nullCell = false;
+                }
+            }
+        }
+
+        /// Alteratively could prevent reallocating using Result.Clear(), however is slow
+        Result = new List<PointValue<byte>>();
+        for (i = 0; i < ocGrid.GetLength(0); i++)
+        {
+            for (j = 0; j < ocGrid.GetLength(1); j++)
+            {
+                if (!ocGrid[i, j].nullCell)
+                    Result.Add(ocGrid[i, j].pv);
+            }
+        }
+        NonOccludedVertices = Result.Count;
+        return Result;
+    }
+    */
+
+    // new Intersection
+    /// <summary>
+    /// Calculates resultant PointValues when 2D Raster (img) is projected along Frustum at Mesh (vertices).
+    /// Mesh represented by list of vertices.
+    /// NOTE 1: does not currently account for occlusion.
+    /// NOTE 2: projection's FOV should be full FOV angles, not half-angles.
+    /// NOTE 3: Attempted to make generic. See note at begining of file.
+    /// </summary>
+    public void Intersection(Frustum projection, List<Vector3> vertices, 
+        out List<Vector3> InView, out List<Vector3> OutView, out List<ViewVector> VV, out List<Vector3> PVecs)
+    {
+        /// setup
+        VerticesInView = 0;
+        InView = new List<Vector3>();
+        OutView = new List<Vector3>();
+        VV = new List<ViewVector>();
+        PVecs = new List<Vector3>();
+
+        /// try each vertex
+        for (i = 0; i < vertices.Count; i++)
+        {
+            /// calculate position vector (world space)
+            Pworld = Vector(projection.Transform.position, vertices[i]);
+
+            /// convert position vector (world space) to position vector (Frustum space)
+            Plocal = projection.Transform.InverseTransformVector(Pworld);
+            PVecs.Add(Plocal);
+
+            /// convert position vector (Frustum space) to view vector (Frustum space)
+            ViewVector Vlocal = new ViewVector(Plocal);
+            VV.Add(Vlocal);
+
+            /// check if view vector is within Frustum FOV
+            if (Math.Abs(Vlocal.Theta) < projection.FOV.Theta / 2.0 &&
+                Math.Abs(Vlocal.Phi) < projection.FOV.Phi / 2.0)
+            {
+                VerticesInView++;
+                InView.Add(vertices[i]);
+            }
+            else
+                OutView.Add(vertices[i]);
+        }
+        InViewCount = InView.Count;
+        OutViewCount = OutView.Count;
+    }
+
+
+    /// <summary>
+    /// Returns vector FROM point1 TO point2.
+    /// </summary>
+    public Vector3 Vector(Vector3 point1, Vector3 point2)
+    {
+        return point2 - point1;
+    }
+
+    /// <summary>
+    /// Converts radians to degrees.
+    /// </summary>
+    public static double RadToDeg(double rad)
+    {
+        return rad * (180.0 / Math.PI);
+    }
+
+    public static double DegToRad(double deg)
+    {
+        return deg * (Math.PI / 180.0);
+    }
+
+    /// <summary>
+    /// Calculates arctan (in radians) so that resultant angle is between -pi and pi.
+    /// </summary>
+    public static double AdjAtanTheta(double numerator, double denominator)
+    {
+        if (denominator < 0 && numerator < 0) /// Q3
+            return -Math.PI / 2.0 - Math.Atan(denominator / numerator);
+        if (numerator > 0 && denominator < 0) /// Q4
+            return Math.PI / 2.0 + Math.Atan(Math.Abs(denominator) / numerator);
+        return Math.Atan(numerator / denominator);
+    }
+
+    /// <summary>
+    /// Calculates arctan (in radians) always in reference to plane of numerator.
+    /// </summary>
+    public static double AdjAtanPhi(Vector3 spatialVector)
+    {
+        Vector3 proj = new Vector3(spatialVector.x, 0, spatialVector.z);
+        float mag = proj.magnitude;
+        return Math.Atan(spatialVector.y / mag);
+    }
+
+    /// <summary>
+    /// Determines the required grid size given hard-coded parameters for occlusion approximation via closest in grid.
+    /// </summary>
+    public Dictionary<string, int> RequiredGrid(ViewVector FOV)
+    {
+        double size = 0.10; // minimum object size to not be considered occluded (m)
+        double angle = 45.0; // maximum surface angle with line-of-sight normal to not be considered occluded (deg)
+        double distance = 3.0; // maximum object distance from sensor to not be considered occluded (m)
+
+        double apparentSize = size * Math.Cos(DegToRad(angle));
+        double totalViewSizeX = 2.0 * distance * Math.Tan(DegToRad(FOV.Theta) / 2.0);
+        double totalViewSizeY = 2.0 * distance * Math.Tan(DegToRad(FOV.Phi) / 2.0);
+
+        Dictionary<string, int> pixels = new Dictionary<string, int>();
+        pixels["i"] = (int)Math.Round(totalViewSizeX / apparentSize);
+        pixels["j"] = (int)Math.Round(totalViewSizeY / apparentSize);
+        return pixels;
+    }
+}
