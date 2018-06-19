ccb626bd517cbd33795f422386e9ab6962021850
mark-scherer <mark.s.scherer@vanderbilt.edu>
2018-06-19 10:31:58 -0500

    added ITv3

---
+++ ITv3/DiagnosticsDriver.cs
@@ -0,0 +1,127 @@
+﻿// Visualizes process diagnostics
+// Mark Scherer, June 2018
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(VertexDriver))]
+public class DiagnosticsDriver : MonoBehaviour
+{
+    // Inspector variables
+    public Material TargetMaterial;
+    public float TargetDistance = 1f;
+    public float TargetSize = 0.01f;
+    public float OcGridSize = 0.005f;
+    public Vector3 DiagnosticsPosition = new Vector3(0, -0.5f, 1);
+    public GameObject DiagnosticsText;
+    public GameObject DiagnosticsBackground;
+
+    // other variables
+    private VertexDriver VD;
+    private Vector3 TargetMin, TargetMax;
+    private float TargetI, TargetJ;
+    private GameObject TargetContainer, OcContainer, Diagnostics;
+    private List<Vector3[]> TargetLines, OcLines;
+    private Intersector Inter;
+
+	
+    // Use this for initialization
+	void Start () {
+        VD = GetComponent<VertexDriver>();
+        Inter = Intersector.Instance;
+
+        // create target
+        TargetI = (float)(TargetDistance * Math.Tan(DegToRad(VD.TargetFOV.x / 2.0)));
+        TargetJ = (float)(TargetDistance * Math.Tan(DegToRad(VD.TargetFOV.y / 2.0)));
+        Vector3 TargetBotLeft = new Vector3(-TargetI, -TargetJ, TargetDistance);
+        Vector3 TargetBotRight = new Vector3(TargetI, -TargetJ, TargetDistance);
+        Vector3 TargetTopLeft = new Vector3(-TargetI, TargetJ, TargetDistance);
+        Vector3 TargetTopRight = new Vector3(TargetI, TargetJ, TargetDistance);
+
+        TargetLines = new List<Vector3[]>();
+        TargetLines.Add(new Vector3[] { TargetBotLeft, TargetBotRight }); // bottom
+        TargetLines.Add(new Vector3[] { TargetBotRight, TargetTopRight }); // right
+        TargetLines.Add(new Vector3[] { TargetTopRight, TargetTopLeft }); // top
+        TargetLines.Add(new Vector3[] { TargetTopLeft, TargetBotLeft }); // left
+
+        OcLines = new List<Vector3[]>();
+        for (int i = 1; i < Inter.OccGridSize.x; i++)
+        {
+            float offset = i * (2f * Math.Abs(TargetI)) / Inter.OccGridSize.x;
+            Vector3 bot = new Vector3(TargetBotLeft.x + offset, TargetBotLeft.y, TargetBotLeft.z);
+            Vector3 top = new Vector3(TargetTopLeft.x + offset, TargetTopLeft.y, TargetTopLeft.z);
+            OcLines.Add(new Vector3[] { bot, top });
+        }
+        for (int i = 1; i < Inter.OccGridSize.y; i++)
+        {
+            float offset = i * (2f * Math.Abs(TargetJ)) / Inter.OccGridSize.y;
+            Vector3 left = new Vector3(TargetBotLeft.x, TargetBotLeft.y + offset, TargetBotLeft.z);
+            Vector3 right = new Vector3(TargetBotRight.x, TargetBotRight.y + offset, TargetBotRight.z);
+            OcLines.Add(new Vector3[] { left, right });
+        }
+
+        TargetContainer = new GameObject();
+        TargetContainer.name = "TargetContainer";
+        for (int i = 0; i < TargetLines.Count; i++)
+        {
+            Vector3[] line = TargetLines[i];
+            VertexDriver.DrawLine(line[0], line[1], TargetContainer, TargetSize, TargetMaterial, i.ToString());
+        }
+        OcContainer = new GameObject();
+        OcContainer.name = "OcContainer";
+        for (int i = 0; i < OcLines.Count; i++)
+        {
+            Vector3[] line = OcLines[i];
+            VertexDriver.DrawLine(line[0], line[1], OcContainer, OcGridSize, TargetMaterial, i.ToString());
+        }
+
+
+        // create diagnostics panel
+        Diagnostics = new GameObject();
+        Diagnostics.transform.position = DiagnosticsPosition;
+        DiagnosticsText.transform.parent = Diagnostics.transform;
+        DiagnosticsText.transform.position = Diagnostics.transform.TransformPoint(DiagnosticsText.transform.position);
+        DiagnosticsBackground.transform.parent = Diagnostics.transform;
+        DiagnosticsBackground.transform.position = Diagnostics.transform.
+            TransformPoint(DiagnosticsBackground.transform.position);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // update target position
+        TargetContainer.transform.position = Camera.main.transform.position;
+        TargetContainer.transform.eulerAngles = Camera.main.transform.eulerAngles;
+        OcContainer.transform.position = Camera.main.transform.position;
+        OcContainer.transform.eulerAngles = Camera.main.transform.eulerAngles;
+
+        for (int i = 0; i < TargetLines.Count; i++)
+        {
+            LineRenderer LR = TargetContainer.transform.Find(i.ToString()).GetComponent<LineRenderer>();
+            LR.SetPosition(0, TargetContainer.transform.TransformPoint(TargetLines[i][0]));
+            LR.SetPosition(1, TargetContainer.transform.TransformPoint(TargetLines[i][1]));
+        }
+        for (int i = 0; i < OcLines.Count; i++)
+        {
+            LineRenderer LR = OcContainer.transform.Find(i.ToString()).GetComponent<LineRenderer>();
+            LR.SetPosition(0, OcContainer.transform.TransformPoint(OcLines[i][0]));
+            LR.SetPosition(1, OcContainer.transform.TransformPoint(OcLines[i][1]));
+        }
+
+        // control diagnostics text
+        String DMessage = String.Format("In View: {0} \t\t Out of View: {1} \t\t Occluded: {2}",
+            VD.Inter.InViewCount, VD.Inter.OutViewCount, VD.Inter.OccludedCount);
+        DiagnosticsText.GetComponent<TextMesh>().text = DMessage;
+    }
+
+    private static double RadToDeg(double rad)
+    {
+        return rad * (180.0 / Math.PI);
+    }
+
+    private static double DegToRad(double deg)
+    {
+        return deg * (Math.PI / 180.0);
+    }
+}
---
+++ ITv3/VertexDriver.cs
@@ -0,0 +1,202 @@
+﻿// Generates random point, visually ID's those ID'd by Intersector as in FOV
+// Mark Scherer, June 2018
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Intersector))]
+public class VertexDriver : MonoBehaviour {
+
+    // Inspector variables 
+    public Vector3 CubeCenter = new Vector3(0, 0, 5);
+    public float CubeSize = 2f;
+    public int Vertices = 100;
+    public float VertexSize = 0.1f;
+    public float LineSize = 0.05f;
+    public Vector2 TargetFOV = new Vector2(10, 10);
+    public Material InViewMaterial;
+    public Material OccludedMaterial;
+    public Material OutViewMaterial;
+    public Material LineMaterial;
+    public bool ViewVectorLabels = true;
+    public GameObject VVLabelText;
+    public GameObject VVLabelBackground;
+    public Vector3 VVLabelOffset = new Vector3(0, 0, -.1f);
+
+    // other variables
+    private ViewVector FOV;
+    public Frustum ViewField { get; private set; }
+    private Vector3 CubeMin, CubeMax;
+    private System.Random Rand = new System.Random();
+    private List<Vector3> Points = new List<Vector3>();
+    private Vector3 Scale;
+    public Intersector Inter { get; private set; }
+    private List<Vector3> InView, OutView, Occluded;
+    private List<ViewVector> VV;
+    private List<Vector3> Pvecs;
+    private GameObject Parent;
+
+
+    // Use this for initialization
+    void Start () {
+        // finish setup
+        FOV = new ViewVector(TargetFOV.x, TargetFOV.y);
+        ViewField = new Frustum(Camera.main.transform, FOV);
+        CubeMin = new Vector3(CubeCenter.x - CubeSize / 2f, CubeCenter.y - CubeSize / 2f,
+            CubeCenter.z - CubeSize / 2f);
+        CubeMax = new Vector3(CubeCenter.x + CubeSize / 2f, CubeCenter.y + CubeSize / 2f,
+            CubeCenter.z + CubeSize / 2f);
+        for (int i = 0; i < Vertices; i++)
+            Points.Add(RandomPoint(CubeMin, CubeMax, Rand));
+        Scale = new Vector3(VertexSize, VertexSize, VertexSize);
+        Inter = Intersector.Instance;
+
+        // create point markers
+        Parent = new GameObject();
+        Parent.name = "VertexMarkers";
+        for (int i = 0; i < Vertices; i++)
+        {
+            GameObject Child = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            Child.AddComponent<MeshFilter>();
+            Child.AddComponent<MeshRenderer>();
+            Child.AddComponent<SphereCollider>();
+            Child.name = "Marker" + i.ToString();
+            Child.transform.parent = Parent.transform;
+            Child.transform.position = Points[i];
+            Child.transform.localScale = Scale;
+            Child.GetComponent<MeshRenderer>().material = OutViewMaterial;
+
+            if (ViewVectorLabels)
+            {
+                GameObject VVLabelT = Instantiate(VVLabelText);
+                VVLabelT.transform.parent = Parent.transform;
+                VVLabelT.name = "Text" + i.ToString();
+                VVLabelT.transform.position = Points[i] + VVLabelOffset;
+                GameObject VVLabelB = Instantiate(VVLabelBackground);
+                VVLabelB.transform.parent = Parent.transform;
+                VVLabelB.name = "Background" + i.ToString();
+                VVLabelB.transform.position = Points[i] + VVLabelOffset + new Vector3(0, 0, 0.01f);
+            }
+        }
+
+        // draw bounds box
+        GameObject BoxParent = new GameObject();
+        BoxParent.name = "BoxParent";
+        DrawBox(CubeMin, CubeMax, BoxParent, LineSize, LineMaterial);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // update ViewField to camera position
+        ViewField.Transform.position = Camera.main.transform.position;
+        ViewField.Transform.eulerAngles = Camera.main.transform.eulerAngles;
+
+        // run intersection analysis
+        Inter.Intersection(ViewField, Points, out InView, out OutView, out Occluded, 
+            out VV, out Pvecs);
+
+        // paint points accordingly
+        for (int i = 0; i < Vertices; i++)
+        {
+            GameObject child = Parent.transform.Find("Marker" + i.ToString()).gameObject;
+            if (InView.FindIndex(x => x == Points[i]) != -1)
+                child.GetComponent<MeshRenderer>().material = InViewMaterial;
+            else if (Occluded.FindIndex(x => x == Points[i]) != -1)
+                child.GetComponent<MeshRenderer>().material = OccludedMaterial;
+            else
+                child.GetComponent<MeshRenderer>().material = OutViewMaterial;
+        }
+
+        if (ViewVectorLabels)
+        {
+            // control View Vector Labels
+            for (int i = 0; i < Points.Count; i++)
+            {
+                Vector3 pt = Points[i];
+                GameObject VVLabelText = Parent.transform.Find("Text" + i.ToString()).gameObject;
+                VVLabelText.GetComponent<TextMesh>().text = viewVectorToStr(VV[i]) + 
+                    "\nP. vector: " + pointToStr(Pvecs[i]);
+            }
+        }
+    }
+
+    private static void DrawBox(Vector3 min, Vector3 max, GameObject parent, float width, Material material)
+    {
+        // create points
+        Vector3 frontbotleft = min;
+        Vector3 frontbotright = new Vector3(max.x, min.y, min.z);
+        Vector3 fronttopleft = new Vector3(min.x, max.y, min.z);
+        Vector3 fronttopright = new Vector3(max.x, max.y, min.z);
+        Vector3 backbotleft = new Vector3(min.x, min.y, max.z);
+        Vector3 backbotright = new Vector3(max.x, min.y, max.z);
+        Vector3 backtopleft = new Vector3(min.x, max.y, max.z);
+        Vector3 backtopright = max;
+
+        // draw front face
+        DrawLine(frontbotleft, frontbotright, parent, width, material);
+        DrawLine(frontbotright, fronttopright, parent, width, material);
+        DrawLine(fronttopright, fronttopleft, parent, width, material);
+        DrawLine(fronttopleft, frontbotleft, parent, width, material);
+        // draw back face
+        DrawLine(backbotleft, backbotright, parent, width, material);
+        DrawLine(backbotright, backtopright, parent, width, material);
+        DrawLine(backtopright, backtopleft, parent, width, material);
+        DrawLine(backtopleft, backbotleft, parent, width, material);
+        // draw connectors
+        DrawLine(frontbotleft, backbotleft, parent, width, material);
+        DrawLine(frontbotright, backbotright, parent, width, material);
+        DrawLine(fronttopright, backtopright, parent, width, material);
+        DrawLine(fronttopleft, backtopleft, parent, width, material);
+    }
+
+    public static void DrawLine(Vector3 start, Vector3 finish, GameObject parent, float width,
+        Material material, string name="")
+    {
+        GameObject Line = new GameObject();
+        Line.name = name;
+        Line.transform.parent = parent.transform;
+        Line.AddComponent(typeof(LineRenderer));
+        LineRenderer DrawnLine = Line.GetComponents<LineRenderer>()[0];
+        DrawnLine.startWidth = 1f;
+        DrawnLine.endWidth = 1f;
+        DrawnLine.widthMultiplier = width;
+        DrawnLine.material = material;
+        Vector3[] LinePoints = new Vector3[] { start, finish };
+        DrawnLine.SetPositions(LinePoints);
+    }
+
+    /// <summary>
+    /// Generates random float from rand between min and max. 
+    /// </summary>
+    private static float RandomFloat(float min, float max, System.Random rand)
+    {
+        double range = max - min;
+        double sample = rand.NextDouble();
+        double scaled = (sample * range) + min;
+        return (float)scaled;
+    }
+
+    /// <summary>
+    /// Generates random point with rand between boundsMin and boundsMax
+    /// </summary>
+    private static Vector3 RandomPoint(Vector3 boundsMin, Vector3 boundsMax, System.Random rand)
+    {
+        return new Vector3(RandomFloat(boundsMin.x, boundsMax.x, rand),
+            RandomFloat(boundsMin.y, boundsMax.y, rand),
+            RandomFloat(boundsMin.z, boundsMax.z, rand));
+    }
+
+    // Returns point coordinates in presentable format.
+    private static string pointToStr(Vector3 point)
+    {
+        return String.Format("({0}, {1}, {2})",
+            Math.Round(point.x, 1), Math.Round(point.y, 1), Math.Round(point.z, 1));
+    }
+
+    private static string viewVectorToStr(ViewVector vector)
+    {
+        return String.Format("(Theta: {0}, Phi: {1})", Math.Round(vector.Theta, 0), Math.Round(vector.Phi, 0));
+    }
+}
