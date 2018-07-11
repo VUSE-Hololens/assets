46b6234f0076a3c3b4a1788a00b6fa965fbec3cf
mark-scherer <mark.s.scherer@vanderbilt.edu>
2018-07-11 15:11:01 -0500

    added proximity sensor

---
+++ Proximity Sensor/EFPDriver.cs
@@ -0,0 +1,218 @@
+﻿/// Driver for EFP (External Feed Pathway)
+/// EFP Tester v2 uses dummy sensor positioned at user's head with simulated data. Does not execute rendering pathway.
+/// Mark Scherer, June 2018
+ 
+/// Notes on dependencies (non-MonoBehavior scripts EFPDriver is dependent on):
+    /// 1. Some have inspector variables (MeshManager). These are implemented as required Components.
+    /// 2. All others are owned as private variables whose source code must be in top-level of assets folder.
+    /// 3. Some are Singletons. Could also use static classes, but seems like Unity prefers Singletons.
+
+/// Note: Would be nice to make switching type of sensor data easier using typedef, but not sure how in C#. 
+/// Currently byte.
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using HoloToolkit.Unity.SpatialMapping;
+
+[RequireComponent(typeof(MeshManager))]
+public class EFPDriver : MonoBehaviour {
+
+    // Inspector variables: misc
+    [Tooltip("Put default material here")]
+    public Material DefaultMaterial;
+    // Inspector variables: sensor specs
+    [Tooltip("FOV of sensor. Degrees.")]
+    public Vector2 SensorFOV = new Vector2(30, 17);
+    [Tooltip("Pixel sount of sensor.")]
+    public Vector2 SensorPixelCount = new Vector2(4096, 3072);
+    // Inspector variables: mesh verte occlusion controls
+    [Tooltip("Minimum actual object size to be considered by occlusion alogrithm. Meters.")]
+    public float OcclusionObjSize = 0.1f;
+    [Tooltip("Distance at which to evaluate object size for occlusion algorithm. Meters.")]
+    public float OcclusionObjDistance = 5f;
+    // Inspector variables: voxel grid controls
+    [Tooltip("Update structure of voxel grid when setting points?")]
+    public bool UpdateVoxStruct = true;
+    [Tooltip("Starting minimum size of voxel. Meters.")]
+    public float DefaultVoxGridRes = 0.1f;
+    [Tooltip("Size of new voxels when growing grid. Meters.")]
+    public float DefaultVoxelSize = 1f;
+    // Inspector variables: mesh vertex rendering controls
+    [Tooltip("Default to rendering non-occluded mesh vertices?")]
+    public bool DefaultRenderVerts = true;
+    [Tooltip("Size of markers of mesh vertices, if rendered. Meters.")]
+    public float VertexMarkerSize = 0.05f;
+    [Tooltip("Color of markers for vertices with min value, if rendered.")]
+    public Color MinColor;
+    [Tooltip("Color of markers for vertices with max value, if rendered.")]
+    public Color MaxColor;
+    // mesh materials
+    [Tooltip("Colored mesh material")]
+    public Material Material1;
+    [Tooltip("Wireframe material")]
+    public Material Material2;
+    // proximity settings
+    public float Closest = 0.5f; // meters
+    public float Furthest = 5; // meters
+
+    // metadata
+    public double DriverSpeed { get; private set; } // Update(), seconds
+    public double MeshManSpeed { get; private set; } // MeshManager:UpdateVertices(), seconds
+    public double IntersectSpeed { get; private set; } // Intersector:Intersection(), seconds
+    public double VoxGridManSpeed { get; private set; } // VoxelGridManager:Set(), seconds
+    public double VertVisSpeed { get; private set; } // VoxelGridManager:Set(), seconds
+    public float MeshDensity { get; private set; } // triangles/m^3
+
+    // dependencies
+    public MeshManager MeshMan { get; private set; }
+    public VoxelGridManager<byte> VoxGridMan { get; private set; }
+    public Visualizer VertVis { get; private set; }
+    public Intersector VertexInter { get; private set; }
+    public SpatialMappingObserver Observer { get; private set; }
+
+    // pre-declarations
+    public Intersector.Frustum SensorField = new Intersector.Frustum();
+    private Stopwatch StopWatch = new Stopwatch();
+    private Stopwatch SubStopWatch = new Stopwatch();
+    public Intersector.Occlusion Oc;
+    private byte[,] SensorData;
+    private List<Visualizer.Content> VertMarkers = new List<Visualizer.Content>();
+
+    // indicator flags
+    private bool VerticesRendered;
+    private bool ColoredMeshShown; // alternative is wireframe mesh
+
+    // control over vertice rendering
+    public bool RenderVerts
+    {
+        get { return VerticesRendered; }
+        set
+        {
+            if (value != VerticesRendered)
+                if (value)
+                    VerticesRendered = true; // vertices created on next Update cycle
+                else
+                {
+                    VertVis.Clear();
+                    VerticesRendered = false;
+                }
+        }
+    }
+
+    // control material of rendered surface mesh
+    public bool ColoredMesh
+    {
+        get { return ColoredMeshShown; }
+        set
+        {
+            if (value != ColoredMeshShown)
+            {
+                if (value)
+                    GetComponent<SpatialMappingManager>().SurfaceMaterial = Material1;
+                else
+                    GetComponent<SpatialMappingManager>().SurfaceMaterial = Material2;
+                ColoredMeshShown = value;
+            }
+        }
+    }
+
+    // control voxel grid resolution
+    public float VoxelGridRes
+    {
+        get { return VoxGridMan.Resolution; }
+        set { VoxGridMan.Resolution = value; }
+    }
+
+    // Use this for initialization
+    void Awake () {
+        // gather dependencies
+        MeshMan = MeshManager.Instance;
+        VoxGridMan = new VoxelGridManager<byte>(myMinSize: DefaultVoxGridRes, myDefaultSize:DefaultVoxelSize);
+        VertVis = new Visualizer("VertexMarkers", "Marker", "Line", DefaultMaterial);
+        VertexInter = new Intersector();
+        Observer = GetComponent<SpatialMappingObserver>();
+
+        // finish setup
+        SensorField.Transform = Camera.main.transform;
+        SensorField.FOV = new Intersector.ViewVector((int)SensorFOV.x, (int)SensorFOV.y);
+        MeshDensity = MeshMan.Density;
+        SensorData = new byte[(int)SensorPixelCount.x, (int)SensorPixelCount.y];
+        for (int i = 0; i < SensorPixelCount.x; i++)
+        {
+            for (int j = 0; j < SensorPixelCount.y; j++)
+                SensorData[i, j] = (byte)(255f * (float)(i + j) / (SensorPixelCount.x + SensorPixelCount.y));
+
+        }
+
+        // finish dependendencies setup: some variables must be created in Start()
+        MeshMan.BoundsVis = new Visualizer("MeshBounds", "Marker", "Line", DefaultMaterial);
+        VerticesRendered = DefaultRenderVerts;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        StopWatch.Reset();
+        StopWatch.Start();
+
+        // update sensor position
+        UpdateSensor(ref SensorField, ref SensorData);
+
+        // update visible mesh vertices
+        SubStopWatch.Reset();
+        SubStopWatch.Start();
+        List<bool> MeshGuide = MeshMan.UpdateVertices(SensorField);
+        MeshDensity = MeshMan.Density;
+        SubStopWatch.Stop();
+        MeshManSpeed = SubStopWatch.ElapsedTicks / (double)Stopwatch.Frequency;
+
+        // project to non-occluded vertices
+        SubStopWatch.Reset();
+        SubStopWatch.Start();
+        Oc = new Intersector.Occlusion(OcclusionObjSize, OcclusionObjDistance, SensorField.FOV);
+        List<Intersector.PointValue<byte>> Updates = 
+            VertexInter.ProjectToVisible(SensorData, SensorField, Oc, Observer.ExtraData, MeshGuide,
+            Closest, Furthest, SensorField.Transform.position);
+        SubStopWatch.Stop();
+        IntersectSpeed = SubStopWatch.ElapsedTicks / (double)Stopwatch.Frequency;
+
+        // update voxel grid
+        SubStopWatch.Reset();
+        SubStopWatch.Start();
+        VoxGridMan.Set(Updates, UpdateVoxStruct);
+        SubStopWatch.Stop();
+        VoxGridManSpeed = SubStopWatch.ElapsedTicks / (double)Stopwatch.Frequency;
+
+        // visualize data
+        SubStopWatch.Reset();
+        SubStopWatch.Start();
+        // render spheres
+        if (RenderVerts)
+        {
+            VertMarkers = Visualizer.CreateMarkers(Updates, 
+                VertexMarkerSize, 0, 255, MinColor, MaxColor);
+            VertVis.Visualize(VertMarkers);
+        }
+        // color mesh
+        if (ColoredMesh)
+        {
+            Visualizer.ColorMesh(Observer.SurfaceObjects, Observer.ExtraData, MeshGuide, 
+                VoxGridMan, MinColor, MaxColor, 0, 255);
+        }
+        SubStopWatch.Stop();
+        VertVisSpeed = SubStopWatch.ElapsedTicks / (double)Stopwatch.Frequency;
+
+        StopWatch.Stop();
+        DriverSpeed = StopWatch.ElapsedTicks / (double)Stopwatch.Frequency;
+	}
+
+    private void UpdateSensor(ref Intersector.Frustum sensorField, ref byte[,] sensorData)
+    {
+        // update position
+        sensorField.Transform.position = Camera.main.transform.position;
+        sensorField.Transform.eulerAngles = Camera.main.transform.eulerAngles;
+
+        // update data
+            // nothing yet
+    }
+}
---
+++ Proximity Sensor/MenuControl.cs
@@ -0,0 +1,212 @@
+﻿/// Menu Control
+/// Backbone for executing results of menu button presses.
+/// Mark Scherer, June 2018
+
+using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.SpatialMapping;
+using UnityEngine;
+
+/// <summary>
+/// Class for executing results of menu button presses.
+/// </summary>
+public class MenuControl : MonoBehaviour
+{
+    // inspector variables: buttons containers
+    [Tooltip("Button Object for diagnostics toggle")]
+    public GameObject DiagButtonContainer;
+    [Tooltip("Button Object for mesh vertices toggle")]
+    public GameObject VertButtonContainer;
+    [Tooltip("Button Object for mesh bounds toggle")]
+    public GameObject BoundsButtonContainer;
+    [Tooltip("Button object for wireframe toggle")]
+    public GameObject MaterialButtonContainer;
+    // sliders
+    public GameObject OcclusionSlider;
+    public GameObject MeshDensitySlider;
+    public GameObject VoxGridMinSizeSlider;
+    public GameObject MeshUpdateTimeSlider;
+    // parent objs
+    [Tooltip("Diagnostics parent GameObject")]
+    public GameObject DiagParent;
+    public GameObject EFP;
+
+
+    // other variables
+    private HoloToolkit.Unity.Buttons.CompoundButton DiagButton;
+    private HoloToolkit.Unity.Buttons.CompoundButton VertButton;
+    private HoloToolkit.Unity.Buttons.CompoundButton BoundsButton;
+    private HoloToolkit.Unity.Buttons.CompoundButton MaterialButton;
+
+    private HoloToolkit.Examples.InteractiveElements.SliderGestureControl OcSliderGC;
+    private HoloToolkit.Examples.InteractiveElements.SliderGestureControl MeshDensitySliderGC;
+    private HoloToolkit.Examples.InteractiveElements.SliderGestureControl VoxGridMinSizeSliderGC;
+    private HoloToolkit.Examples.InteractiveElements.SliderGestureControl MeshUpdateTimeSliderGC;
+
+    private void Start()
+    {
+        // grab button component
+        DiagButton = DiagButtonContainer.GetComponent<HoloToolkit.Unity.Buttons.CompoundButton>();
+        VertButton = VertButtonContainer.GetComponent<HoloToolkit.Unity.Buttons.CompoundButton>();
+        BoundsButton = BoundsButtonContainer.GetComponent<HoloToolkit.Unity.Buttons.CompoundButton>();
+        MaterialButton = MaterialButtonContainer.GetComponent<HoloToolkit.Unity.Buttons.CompoundButton>();
+
+        // declare event handlers
+        DiagButton.OnButtonPressed += new System.Action<GameObject>(ToggleDiag);
+        VertButton.OnButtonPressed += new System.Action<GameObject>(ToggleVerts);
+        BoundsButton.OnButtonPressed += new System.Action<GameObject>(ToggleBounds);
+        MaterialButton.OnButtonPressed += new System.Action<GameObject>(ToggleMaterial);
+
+        // add sliders as listeners
+        OcSliderGC = OcclusionSlider.GetComponent<HoloToolkit.Examples.InteractiveElements.SliderGestureControl>();
+        OcSliderGC.OnUpdateEvent.AddListener(UpdateOc);
+        MeshDensitySliderGC = MeshDensitySlider.GetComponent<HoloToolkit.Examples.InteractiveElements.SliderGestureControl>();
+        MeshDensitySliderGC.OnUpdateEvent.AddListener(UpdateMeshDensity);
+        VoxGridMinSizeSliderGC = VoxGridMinSizeSlider.GetComponent<HoloToolkit.Examples.InteractiveElements.SliderGestureControl>();
+        VoxGridMinSizeSliderGC.OnUpdateEvent.AddListener(UpdateVoxGrid);
+        MeshUpdateTimeSliderGC = MeshUpdateTimeSlider.GetComponent<HoloToolkit.Examples.InteractiveElements.SliderGestureControl>();
+        MeshUpdateTimeSliderGC.OnUpdateEvent.AddListener(UpdateMeshUpdateTime);
+
+        // set original button labels
+        UpdateDiagLabel();
+        UpdateVertLabel();
+        UpdateBoundsLabel();
+        UpdateMaterialLabel();
+
+        // finish syncing state
+        UpdateOc(OcSliderGC.SliderValue);
+        UpdateMeshDensity(MeshDensitySliderGC.SliderValue);
+        UpdateVoxGrid(VoxGridMinSizeSliderGC.SliderValue);
+        UpdateMeshUpdateTime(MeshUpdateTimeSliderGC.SliderValue);
+    }
+
+    /// <summary>
+    /// Toggle visibility of diagnostics board.
+    /// </summary>
+    private void ToggleDiag(GameObject button)
+    {
+        DiagParent.GetComponent<DiagnosticsControl>().ShowBoard = 
+            !DiagParent.GetComponent<DiagnosticsControl>().ShowBoard;
+
+        UpdateDiagLabel();
+    }
+
+    /// <summary>
+    /// Updates diagnostic toggle button label to current diagnostics board state.
+    /// </summary>
+    private void UpdateDiagLabel()
+    {
+        string On = "Hide Diagnostics";
+        string Off = "Show Diagnostics";
+
+        if (DiagParent.GetComponent<DiagnosticsControl>().ShowBoard)
+            DiagButtonContainer.transform.Find("Text").GetComponent<TextMesh>().text = On;
+        else
+            DiagButtonContainer.transform.Find("Text").GetComponent<TextMesh>().text = Off;
+    }
+
+    /// <summary>
+    /// Toggle visibility of diagnostics board.
+    /// </summary>
+    private void ToggleVerts(GameObject button)
+    {
+        EFP.GetComponent<EFPDriver>().RenderVerts = !EFP.GetComponent<EFPDriver>().RenderVerts;
+
+        UpdateVertLabel();
+    }
+
+    /// <summary>
+    /// Updates mesh vertices toggle button label to current state.
+    /// </summary>
+    private void UpdateVertLabel()
+    {
+        string On = "Hide Vertices";
+        string Off = "Show Vertices";
+
+        if (EFP.GetComponent<EFPDriver>().RenderVerts)
+            VertButtonContainer.transform.Find("Text").GetComponent<TextMesh>().text = On;
+        else
+            VertButtonContainer.transform.Find("Text").GetComponent<TextMesh>().text = Off;
+    }
+
+    /// <summary>
+    /// Toggle visibility of diagnostics board.
+    /// </summary>
+    private void ToggleBounds(GameObject button)
+    {
+        EFP.GetComponent<EFPDriver>().MeshMan.VB = !EFP.GetComponent<EFPDriver>().MeshMan.VB;
+
+        UpdateBoundsLabel();
+    }
+
+    /// <summary>
+    /// Updates mesh bounds toggle button label to current state.
+    /// </summary>
+    private void UpdateBoundsLabel()
+    {
+        string On = "Hide Mesh Bounds";
+        string Off = "Show Mesh Bounds";
+
+        if (EFP.GetComponent<EFPDriver>().MeshMan.VB)
+            BoundsButtonContainer.transform.Find("Text").GetComponent<TextMesh>().text = On;
+        else
+            BoundsButtonContainer.transform.Find("Text").GetComponent<TextMesh>().text = Off;
+    }
+
+    /// <summary>
+    /// Toggles material for mesh rendering.
+    /// </summary>
+    private void ToggleMaterial(GameObject button)
+    {
+        EFP.GetComponent<EFPDriver>().ColoredMesh = !EFP.GetComponent<EFPDriver>().ColoredMesh;
+
+        UpdateMaterialLabel();
+    }
+
+    /// <summary>
+    /// Updates wireframe toggle button label to current state.
+    /// </summary>
+    private void UpdateMaterialLabel()
+    {
+        string Material1 = "Wireframe";
+        string Material2 = "Colored Mesh";
+
+        if (EFP.GetComponent<EFPDriver>().ColoredMesh)
+            MaterialButtonContainer.transform.Find("Text").GetComponent<TextMesh>().text = Material1;
+        else
+            MaterialButtonContainer.transform.Find("Text").GetComponent<TextMesh>().text = Material2;
+    }
+
+    /// <summary>
+    /// Updates occlusion object size with slider value.
+    /// </summary>
+    private void UpdateOc(float value)
+    {
+        EFP.GetComponent<EFPDriver>().OcclusionObjSize = value / 100;
+    }
+
+    /// <summary>
+    /// Updates mesh visibility FOV factor with slider value.
+    /// </summary>
+    private void UpdateMeshDensity(float value)
+    {
+        EFP.GetComponent<SpatialMappingObserver>().Density = value;
+    }
+
+    /// <summary>
+    /// Updates voxel grid with new minimum size.
+    /// </summary>
+    private void UpdateVoxGrid(float value)
+    {
+        EFP.GetComponent<EFPDriver>().VoxelGridRes = value;
+    }
+
+    /// <summary>
+    /// Updates mesh refresh cyle time.
+    /// </summary>
+    private void UpdateMeshUpdateTime(float value)
+    {
+        EFP.GetComponent<SpatialMappingObserver>().TimeBetweenUpdates = value;
+    }
+}
