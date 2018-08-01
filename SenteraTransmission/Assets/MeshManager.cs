0d10c9de0322b58d739096cd7177a2126f18bcb9
hbristol <henry.bristol@vanderbilt.edu>
2018-08-01 08:59:21 -0500

    sentera transmission added and tested

---
+++ SenteraTransmission/Assets/MeshManager.cs
@@ -0,0 +1,177 @@
+﻿/// MeshManager
+/// Interface for Hololens spatial mapping data via HoloToolKit/SpatialMapping/SpatialMappingObserver.
+/// Singleton - ALWAYS access via Instance. NEVER use constructor.
+/// Mark Scherer, June 2018
+
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using HoloToolkit.Unity.SpatialMapping;
+
+/// <summary>
+/// Accessor class to Hololens spatial mapping data via HoloToolKit/SpatialMapping/SpatialMappingObserver.
+/// </summary>
+[RequireComponent(typeof(HoloToolkit.Unity.SpatialMapping.SpatialMappingObserver))]
+public class MeshManager : HoloToolkit.Unity.Singleton<MeshManager>
+{
+    /// <summary>
+    /// Metadata: number of independent meshes returned by SpatialMappingManager.
+    /// NOTE: only updated when getVertices() is called.
+    /// </summary>
+    public int TotalMeshCount { get; private set; }
+
+    /// <summary>
+    /// Metadata: number of independent visible meshes.
+    /// NOTE: only updated when getVertices() is called.
+    /// </summary>
+    public int MeshesInView { get; private set; }
+
+    /// <summary>
+    /// Metadata: number of triangles in all meshes.
+    /// NOTE: only updated when getVertices() is called.
+    /// </summary>
+    public int TotalTriangleCount { get; private set; }
+
+    /// <summary>
+    /// Metadata: number of triangles in all visible meshes.
+    /// NOTE: only updated when getVertices() is called.
+    /// </summary>
+    public int TrianglesInView { get; private set; }
+
+    /// <summary>
+    /// Metadata: number of vertices in all meshes.
+    /// NOTE: only updated when getVertices() is called.
+    /// </summary>
+    public int TotalVertexCount { get; private set; }
+
+    /// <summary>
+    /// Metadata: number of vertices in all visible meshes.
+    /// NOTE: only updated when getVertices() is called.
+    /// </summary>
+    public int VerticesInView { get; private set; }
+
+    /// <summary>
+    /// Density of mesh data in triangles / m^3
+    /// </summary>
+    public float Density { get; private set; }
+
+    // other variables
+    private SpatialMappingObserver observer;
+    private Intersector MeshInter;
+
+    void Start()
+    {
+        observer = GetComponent<SpatialMappingObserver>();
+        Density = observer.Density;
+        MeshInter = new Intersector();
+
+        TotalMeshCount = 0;
+        MeshesInView = 0;
+        TrianglesInView = 0;
+        VerticesInView = 0;
+    }
+
+    /// <summary>
+    /// ID's all of SpatialMappingObserver's store meshes as visible or not, updates class metadata.
+    /// Renders mesh bounds if VisualizeBounds.
+    /// </summary>
+    public List<bool> UpdateVertices(Intersector.Frustum SensorView)
+    {
+        Density = observer.Density;
+
+        // create fresh lists, metadata
+        List<bool> visiblilty = new List<bool>();
+        TotalMeshCount = 0;
+        TotalTriangleCount = 0;
+        TotalVertexCount = 0;
+        MeshesInView = 0;
+        TrianglesInView = 0;
+        VerticesInView = 0;
+
+        // check meshes for visiblity
+        for (int i = 0; i < observer.ExtraData.Count; i++)
+        {
+            SurfacePoints extras = observer.ExtraData[i];
+            bool isVisible = MeshInter.AnyInView(extras.IntersectPts, SensorView);
+
+            visiblilty.Add(isVisible);
+            
+            if (isVisible)
+            {
+                // update metadata
+                MeshesInView++;
+                TrianglesInView += extras.TriangleCount;
+                VerticesInView += extras.Wvertices.Count;
+            }
+            // update metadata totals
+            TotalMeshCount++;
+            TotalTriangleCount += extras.TriangleCount;
+            TotalVertexCount += extras.Wvertices.Count;
+        }
+
+        return visiblilty;
+    }
+    
+    /// <summary>
+    /// Returns bounds of all stored meshes
+    /// </summary>
+    public List<Bounds> AllMeshBounds()
+    {
+        List<Bounds> MeshBounds = new List<Bounds>();
+        for (int i = 0; i < observer.ExtraData.Count; i++)
+            MeshBounds.Add(observer.ExtraData[i].BoundsBox);
+        return MeshBounds;
+    }
+
+    /// <summary>
+    /// Assembles bounding points for mesh defined by bounds.
+    /// </summary>
+    public static List<Vector3> IntersectionPoints(Bounds bounds)
+    {
+        List<Vector3> pts = new List<Vector3>();
+        pts.AddRange(Corners(bounds));
+        return pts;
+    }
+
+    /// <summary>
+    /// Returns list of corner vertices from Axis Aligned Bounding Box
+    /// </summary>
+    private static List<Vector3> Corners(Bounds bounds)
+    {
+        Vector3 Wmin = bounds.min;
+        Vector3 Wmax = bounds.max;
+
+        List<Vector3> corners = new List<Vector3>();
+        corners.Add(Wmin); // front bottom left
+        corners.Add(new Vector3(Wmax.x, Wmin.y, Wmin.z)); // front bottom right
+        corners.Add(new Vector3(Wmax.x, Wmax.y, Wmin.z)); // front top right
+        corners.Add(new Vector3(Wmin.x, Wmax.y, Wmin.z)); // front top left
+        corners.Add(new Vector3(Wmin.x, Wmin.y, Wmax.z)); // back bottom left
+        corners.Add(new Vector3(Wmax.x, Wmin.y, Wmax.z)); // back bottom right
+        corners.Add(Wmax); // back top right
+        corners.Add(new Vector3(Wmin.x, Wmax.y, Wmax.z)); // back top left
+        return corners;
+    }
+
+    /// <summary>
+    /// Returns list of face vertices from Axis Aligned Bounding Box
+    /// </summary>
+    private static List<Vector3> FacePoints(Bounds bounds)
+    {
+        Vector3 Wcenter = bounds.center;
+        List<Vector3> points = new List<Vector3>();
+        Vector3 adj = new Vector3(0, 0, -bounds.extents.z); // front
+        points.Add(Wcenter + adj);
+        adj = new Vector3(0, 0, bounds.extents.z); // back
+        points.Add(Wcenter + adj);
+        adj = new Vector3(0, bounds.extents.y, 0); // top
+        points.Add(Wcenter + adj);
+        adj = new Vector3(0, -bounds.extents.y, 0); // bottom
+        points.Add(Wcenter + adj);
+        adj = new Vector3(-bounds.extents.x, 0, 0); // left
+        points.Add(Wcenter + adj);
+        adj = new Vector3(bounds.extents.x, 0, 0); // right
+        points.Add(Wcenter + adj);
+        return points;
+    }
+}
---
+++ SenteraTransmission/Assets/SliderTester.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+/// <summary>
+/// Tests slider in unity by forcing motion.
+/// </summary>
+public class SliderTester : MonoBehaviour
+{
+
+    // inspector variables
+    public bool Enabled = false;
+    public GameObject Slider;
+    public float Cycle = 2f;
+
+    // other vars
+    private HoloToolkit.Examples.InteractiveElements.SliderGestureControl SliderGC;
+    private Stopwatch Watch = new Stopwatch();
+
+    // Use this for initialization
+    void Start()
+    {
+        SliderGC = Slider.GetComponent<HoloToolkit.Examples.InteractiveElements.SliderGestureControl>();
+
+        Watch.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = (float)Watch.ElapsedTicks / (float)Stopwatch.Frequency;
+        float value = SliderGC.MinSliderValue + (time / Cycle) * (SliderGC.MaxSliderValue - SliderGC.MinSliderValue);
+
+        if (Enabled)
+            SliderGC.SetSliderValue(value);
+
+        if (time > Cycle)
+        {
+            Watch.Reset();
+            Watch.Start();
+        }
+    }
+}
