8c9bda0d1174c562b1c7305d1ab0a108541f6477
mark-scherer <mark.s.scherer@vanderbilt.edu>
2018-09-21 15:13:38 -0500

    added NDVIConfig

---
+++ NDVIConfig/ReceiverDriver.cs
@@ -0,0 +1,58 @@
+﻿// ReceiverDriver.cs
+// Shell class to hold receiver object and its inspector variables.
+// Mark Scherer, September 2018
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceiverDriver : MonoBehaviour
+{
+    // inspector vars
+    public string remoteIP = "10.67.134.150";
+    public string remotePrimPort = "8888";
+    public string remoteSecPort = "8889";
+
+    // other vars
+    public Receiver rec;
+    public byte[] dummyData { get; private set; }
+    public int dummySize = 25;
+    public float period = 3;
+    public float periodStart = 0;
+
+    // Use this for initialization
+    void Awake()
+    {
+        rec = new Receiver(remoteIP, remotePrimPort, remoteSecPort);
+
+        dummyData = new byte[dummySize * dummySize];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rec.CurrentHash == Receiver.INVALID_DATA_HASH)
+        {
+            UpdateDummy();
+        }
+    }
+
+    private void UpdateDummy()
+    {
+        float curTime = Time.time;
+        if (curTime - periodStart > period)
+        {
+            periodStart = curTime;
+        }
+
+        float offset = (curTime - periodStart) / period;
+
+        for (int i = 0; i < dummySize * dummySize; i++)
+        {
+            //float col = i % dummySize;
+            //dummyData[i] = (byte)((byte)255 + (byte)(255f/2f * Math.Sin(2*Math.PI * (col + offset) / dummySize/2)));
+            dummyData[i] = (byte)(255 / 2 + 255f / 2f * Math.Sin(2 * Math.PI * offset));
+        }
+    }
+}
---
+++ NDVIConfig/Serializer.cs
@@ -0,0 +1,43 @@
+﻿// Serializer.cs
+// class for serializing and deserializing data
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Serializer {
+    // int[]
+    public byte[] Serialize(int[] content)
+    {
+        byte[] bytes = new byte[content.Length * 4];
+
+        for (int i = 0; i < content.Length; i++)
+        {
+            byte[] tmp = BitConverter.GetBytes(content[i]);
+
+            for (int j = 0; j < 4; j++)
+                bytes[i * 4 + j] = tmp[j];
+        }
+
+        return bytes;
+    }
+
+    public int[] DeserializeInt(byte[] bytes)
+    {
+        int[] content = new int[bytes.Length / 4];
+        byte[] _bytes = new byte[4];
+
+        for (int i = 0; i < bytes.Length / 4; i++)
+        {
+            // reverse endianess
+            Array.Copy(bytes, i * 4, _bytes, 0, 4);
+            Array.Reverse(_bytes);
+
+            // convert to int
+            content[i] = BitConverter.ToInt32(_bytes, 0);
+        }
+
+        return content;
+    }
+}
